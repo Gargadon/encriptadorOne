60f81a5b29c42227d90b761eaa412fa881c2e587
David Kantun <dkantun@gmail.com>
2024-08-13 13:50:52 -0600

    Primer intento

---
+++ encriptar.js
@@ -0,0 +1,68 @@
+function encripta(texto) {
+  if (/[^a-z\s]/.test(texto)) {
+    alertaError();
+  }
+
+  const conversiones = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+  };
+
+  texto = texto.replace(/[aeiou]/g, (match) => conversiones[match]);
+  return texto;
+}
+
+function desencripta(texto) {
+  if (/[^a-z\s]/.test(texto)) {
+    alertaError();
+  }
+
+  const conversiones = [
+    ["ai", "a"],
+    ["enter", "e"],
+    ["imes", "i"],
+    ["ober", "o"],
+    ["ufat", "u"],
+  ];
+
+  for (let [buscar, reemplazar] of conversiones) {
+    const regex = new RegExp(buscar, "g");
+    texto = texto.replace(regex, reemplazar);
+  }
+
+  return texto;
+}
+
+function encriptaTexto(event) {
+  event.preventDefault();
+  const texto = document.getElementById("texto").value;
+  const resultado = encripta(texto);
+  document.getElementById("resultado").textContent = resultado;
+  botonCopiar();
+}
+
+function desencriptaTexto(event) {
+  event.preventDefault();
+  const texto = document.getElementById("texto").value;
+  const resultado = desencripta(texto);
+  document.getElementById("resultado").textContent = resultado;
+  botonCopiar();
+}
+
+function alertaError() {
+  alert("El texto tiene caracteres no permitidos.");
+  die();
+}
+
+function botonCopiar() {
+  const boton = document.getElementById("botonCopiar");
+  boton.style.display = "block";
+}
+
+function copiar() {
+  const resultado = document.getElementById("resultado").textContent;
+  navigator.clipboard.writeText(resultado);
+}
